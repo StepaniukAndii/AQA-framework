a65e44624a2acc23cb6d7728127071558f1a519e
Andrii <morozok966@gmail.com>
2022-05-30 13:26:51 +0300

    Add heandless and create class RozetkaUaTest

--- src/test/java/test/AbstractTestInit.java
+++ src/test/java/test/AbstractTestInit.java
@@ -3,15 +3,26 @@ package test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class AbstractTestInit {
+
     WebDriver driver;
 
     @BeforeMethod
     public void setup(){
+        String headless = System.getProperty("headless");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 }
 
---
+++ src/test/java/test/RozetkaUaTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class RozetkaUaTest extends AbstractTestInit{
+
+    @Test
+    public void rozetkaUatest(){
+
+    }
+}
