ae2aae743e9f3771f2e301cb4f7f19404aae4035
Oleksandr Serhadov <sergadovlol@gmail.com>
2022-05-30 16:22:33 +0300

    w8 for click

--- src/test/java/pageObject/BasePage.java
+++ src/test/java/pageObject/BasePage.java
@@ -16,8 +16,12 @@ public class BasePage {
         this.driver = driver;
     }
 
-    public WebElement weitSetup(String locator) {
+    public WebElement weitVisibleOfElement(String locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+    public WebElement weitForClick(String locator) {
+        WebDriverWait waitClick = new WebDriverWait(driver, Duration.ofSeconds(15));
+        return waitClick.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
     }
 }
