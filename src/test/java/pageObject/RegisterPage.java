20af707632a5054380ef11a554d6b67c77fc8146
Oleksandr Serhadov <sergadovlol@gmail.com>
2022-05-31 14:27:49 +0300

    add 2 class for test 'demo web' and add 2 metods in Init

--- src/test/java/pageObject/RegisterPage.java
+++ src/test/java/pageObject/RegisterPage.java
@@ -1,4 +1,51 @@
 package pageObject;
 
-public class RegisterPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RegisterPage extends BasePage {
+    public RegisterPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToTestShop() {
+        driver.get("http://demowebshop.tricentis.com/");
+    }
+
+    public WebElement clickReg() {
+        return weitForClick("//a[contains(@href,\"/register\")]");
+    }
+
+    public WebElement radioMale() {
+        return weitForClick("//*[contains(@id,\"gender-female\")]");
+    }
+
+    public WebElement getEntrFirstName() {
+        return weitVisibleOfElement("//*[contains(@id,\"FirstName\")]");
+    }
+
+    public WebElement getEntrLastName() {
+        return weitVisibleOfElement("//*[contains(@id,\"LastName\")]");
+    }
+
+    public WebElement getEntrEmail() {
+        return weitVisibleOfElement("//*[contains(@id,\"Email\")]");
+    }
+
+    public WebElement getEntrPass() {
+        return weitVisibleOfElement("//*[@id=\"Password\"]");
+    }
+
+    public WebElement getEntrConfirmPass() {
+        return weitVisibleOfElement("//*[@id=\"ConfirmPassword\"]");
+    }
+
+    public WebElement getRegisterBtn() {
+        return weitVisibleOfElement("//*[@id=\"register-button\"]");
+    }
+
+    public void getRegisterPage() {
+        driver.get("http://demowebshop.tricentis.com/registerresult/1");
+    }
 }
--- src/test/java/test/TestSingUp.java
+++ src/test/java/test/TestSingUp.java
@@ -1,4 +1,24 @@
 package test;
 
-public class TestSingUp {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObject.RegisterPage;
+
+public class TestSingUp extends AbstractTestInit{
+
+    @Test
+    public void testRegister(){
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.goToTestShop();
+        registerPage.clickReg().click();
+        registerPage.radioMale().click();
+        registerPage.getEntrFirstName().sendKeys(createRandomNumber() + "TestUserName");
+        registerPage.getEntrLastName().sendKeys(createRandomNumber() + "TestUserLastName");
+        registerPage.getEntrEmail().sendKeys(createRandomNumber() + "sergadovlol@gmail.com");
+        registerPage.getEntrPass().sendKeys("ewqasdcxz");
+        registerPage.getEntrConfirmPass().sendKeys("ewqasdcxz");
+        registerPage.getRegisterBtn().click();
+
+        Assert.assertTrue(getUrl().contains("http://demowebshop.tricentis.com/registerresult/1"));
+    }
 }
