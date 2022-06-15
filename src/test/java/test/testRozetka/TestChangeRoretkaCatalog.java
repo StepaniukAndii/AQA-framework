99ac19462944d7f0a846e2ef0cc6188511ecf847
Andrii Ivachov <ivchov9898@gmail.com>
2022-06-15 14:56:49 +0300

    refactoring code

--- src/test/java/test/testRozetka/TestChangeRoretkaCatalog.java
+++ src/test/java/test/testRozetka/TestChangeRoretkaCatalog.java
@@ -1,9 +1,7 @@
 package test.testRozetka;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.reporters.jq.Main;
 import page.ChangeCatalogRozetka;
 import page.HomeElementRozetka;
 import test.AbstractTestInit;
@@ -18,25 +16,10 @@ public class TestChangeRoretkaCatalog extends AbstractTestInit {
         changeCatalogRozetka.getSearchLine().sendKeys("Холодильник\n");
         HomeElementRozetka homeElementRozetka = new HomeElementRozetka(driver);
         int smallIconCatalog = homeElementRozetka.getCatalogElement().getSize().getWidth();
-////      driver.findElement(By.xpath("//ul[@class=\"catalog-grid ng-star-inserted\"]/li")).getSize().getWidth();
-//        driver.findElement(By.xpath("//ul[@class=\"catalog-grid ng-star-inserted\"]/li")).getSize().getWidth();
         changeCatalogRozetka.getClickableElement("//button[@class='catalog-view__button ng-star-inserted']").click();
         int bigIconCatalog = homeElementRozetka.getCatalogElement().getSize().getWidth();
 
-//        driver.findElement(By.xpath("//li[@class=\"catalog-grid__cell ng-star-inserted\"]")).getSize().getWidth();
-//        //ввести очыкувану ширину ы фактичну. після кліка ширина має бути більша
-////
-//        Assert.assertEquals(homeElementRozetka.getOne(), homeElementRozetka.getTwo());
         Assert.assertTrue(smallIconCatalog < bigIconCatalog,
                 "I expect catalog to change object size");
-
     }
-
-
-//    public void getChangeCatalog(){
-//        ChangeCatalogRozetka changeCatalogRozetka = new ChangeCatalogRozetka(driver);
-//        changeCatalogRozetka.goToRozetkaUa();
-//        changeCatalogRozetka.getSearchLine().sendKeys("Холодильник\n");
-//        changeCatalogRozetka.getClickableElement("//button[@class=\"catalog-view__button ng-star-inserted\"]");
-//    }
 }
