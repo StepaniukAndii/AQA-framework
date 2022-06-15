05f069e38c49f661c3dafcb4eb2987ae76cd4ed2
Andrii Ivachov <ivchov9898@gmail.com>
2022-06-15 16:05:35 +0300

    add new test testList

--- src/test/java/page/RozetkaSortList.java
+++ src/test/java/page/RozetkaSortList.java
@@ -1,2 +1,30 @@
-package page;public class RozetkaSortList {
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class RozetkaSortList extends HomeElementRozetka{
+    public RozetkaSortList(WebDriver driver) {
+        super(driver);
+    }
+//    public void getListItems(){
+//        RozetkaSortList rozetkaSortList = new RozetkaSortList(driver);
+//        rozetkaSortList.goToRozetkaUa();
+////        List<WebElement> productList = driver.findElements(By.xpath("//li[@class=\"catalog-grid__cell catalog-grid__cell_type_slim ng-star-inserted"));
+////        int priceProduct = productList.
+//
+//    }
+
+    public void clickSortPrice(){
+        WebElement dropDown = getClickableElement("//select");
+        Select select = new Select(dropDown);
+        select.selectByIndex(1);
+    }
+
+
+
 }
--- src/test/java/test/testRozetka/TestChangePriceLowToHide.java
+++ src/test/java/test/testRozetka/TestChangePriceLowToHide.java
@@ -1,2 +1,15 @@
-package test.testRozetka;public class TestChangePriceLowToHide {
+package test.testRozetka;
+
+import org.testng.annotations.Test;
+import page.RozetkaSortList;
+import test.AbstractTestInit;
+
+public class TestChangePriceLowToHide extends AbstractTestInit {
+
+    @Test
+    public void checkChangeSortPrice(){
+       RozetkaSortList rozetkaSortList = new RozetkaSortList(driver);
+       rozetkaSortList.goToRozetkaUa();
+       rozetkaSortList.getPrintInSearchLine("Ноутбук");
+    }
 }
